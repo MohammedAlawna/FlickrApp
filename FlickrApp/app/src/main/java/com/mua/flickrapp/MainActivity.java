09dd4f679bc9f7fd850f578692a5755f592bac16
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2021-10-22 23:48:36 -0700

    addedSimpleLogs.

--- FlickrApp/app/src/main/java/com/mua/flickrapp/MainActivity.java
+++ FlickrApp/app/src/main/java/com/mua/flickrapp/MainActivity.java
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -19,12 +20,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: starts");
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
@@ -43,12 +46,14 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Log.d(TAG, "onCreate: ends");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.d(TAG, "onCreateOptionsMenu() returned: " + true);
         return true;
     }
 
@@ -64,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+        Log.d(TAG, "onOptionsItemSelected: returned");
         return super.onOptionsItemSelected(item);
     }
 
