08e3676b5157fadfc0d90d3edfb8373ca1b1b93c
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2021-10-23 23:24:41 -0700

    1

--- FlickrApp/app/src/main/java/com/mua/flickrapp/GetRawData.java
+++ FlickrApp/app/src/main/java/com/mua/flickrapp/GetRawData.java
@@ -3,7 +3,11 @@ package com.mua.flickrapp;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.loader.content.AsyncTaskLoader;
+
+import java.io.BufferedReader;
+import java.net.HttpURLConnection;
 
 enum DownloadStatus {IDLE, PROCESSING, NOT_INSTALLED, FAILED_OR_EMPTY, OK}
 
@@ -14,5 +18,18 @@ class GetRawData extends AsyncTaskLoader<String,Void,String> {
 
     public GetRawData() {
         this.mDownloadStatus = DownloadStatus.IDLE;
+    }
+
+
+    @Nullable
+    @Override
+    public String loadInBackground(String... strings) {
+        HttpURLConnection urlCon = null;
+        BufferedReader reader = null;
+
+        if(strings == null) {
+           mDownloadStatus = DownloadStatus.NOT_INSTALLED;
+        }
+        return null;
     }
 }
